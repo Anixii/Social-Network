0984b4183309ff54f5c876ce2c64892f648b3e5a
Anixii <kenjebekoverbol53@gmail.com>
2023-04-16 22:52:52 +0600

    63.1

--- src/api/api.js
+++ src/api/api.js
@@ -18,13 +18,21 @@ export const userAPI = {
 } 
 export const authAPI = {  
    logIn(){ 
-    return instance.get(`auth/me`, { 
-        withCredentials: true
-    })   
+    return instance.get(`auth/me`)   
    }
+}  
+export const followAPI = { 
+    onFollow(id){ 
+        return instance.post(`follow/${id}`, {},) 
+    }, 
+    unFollow(id){ 
+        return instance.delete(`follow/${id}`) 
+    }
 } 
+
+
 // axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, { 
 //     withCredentials: true
 // }) 
-
-
+// axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${item.id}`, {},{ withCredentials:true, headers: {'API-KEY': '0adaae07-4e14-40b8-b038-30ec25a5b1cd'}})
+//axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${item.id}`, { withCredentials:true,headers: {'API-KEY': '0adaae07-4e14-40b8-b038-30ec25a5b1cd'}}) 
