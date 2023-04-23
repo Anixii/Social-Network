4c15f41cb7347377926fee5ab9e671d6b1df7bf8
Anixii <kenjebekoverbol53@gmail.com>
2023-04-23 22:39:37 +0600

    71. ProfileStatus completed

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,13 +3,20 @@ import React from "react"
 class ProfileStatus extends React.Component { 
     state ={ 
         editMode: false,
+    } 
+    activedEditMode = ()=> {  
+        this.setState({editMode: true}) 
+
+    } 
+    deactivedEditMode = ()=>{ 
+        this.setState({editMode: false}) 
     }
     render(){ 
 
         return( 
             <div> 
-                {!this.state.editMode &&  <div>{this.props.status}</div>} 
-                {this.state.editMode &&  <div><input type="text" value={this.props.status} /></div>} 
+                {!this.state.editMode &&  <div onDoubleClick={this.activedEditMode}>{this.props.status}</div>} 
+                {this.state.editMode &&  <div><input autoFocus={true} onBlur={this.deactivedEditMode} type="text" value={this.props.status} /></div>} 
             
         </div>
     )
