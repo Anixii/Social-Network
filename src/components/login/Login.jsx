e955c0f132b8d7e11625d5eb253e40de9dde4f38
Anixii <kenjebekoverbol53@gmail.com>
2023-04-25 23:32:54 +0600

    75. ready

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,22 @@
 import { Field, reduxForm } from "redux-form"
 
 const Login =(props) =>{ 
-     
+     const onSubmit = (formData) =>{ 
+        console.log(formData)
+     }
     return(  
         <div> 
 
         <div>Login</div>
          
-       <ReduxLoginForm/>
+       <ReduxLoginForm onSubmit={onSubmit}/>
          
         </div>
     )
 }  
 const LoginForm = (props) =>{ 
     return( 
-        <form action=""> 
+        <form onSubmit={props.handleSubmit}> 
         <div> <Field name={'login'} type="text" placeholder="Login" component={'input'} /></div> 
         <div><Field name={"password"} type="password" placeholder="Password" component={'input'}/></div>
         <div><Field name={"rememberMe"} type="checkbox" component={'input'}/> Remember me</div>
