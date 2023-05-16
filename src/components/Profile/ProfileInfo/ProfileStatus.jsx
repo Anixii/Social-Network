01a5603bc3db3ea497cbcd2db3ef305aeb1f605d
Anixii <kenjebekoverbol53@gmail.com>
2023-05-16 23:37:35 +0600

    85.ready

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,39 +1,5 @@
-// import React from "react"
-import { useEffect, useState } from "react"
-// class ProfileStatus extends React.Component { 
-    
-//     state ={ 
-//         editMode: false,  
-//         status: this.props.status
-//     } 
-//     activedEditMode = ()=> {  
-//         this.setState({editMode: true}) 
-//     } 
-//     deactivedEditMode = ()=>{ 
-//         this.setState({editMode: false}) ;
-//         this.props.updateStatusThunkC(this.state.status)
-//     } 
-//     onStatusChange = (e) =>{ 
-//         this.setState({status: e.currentTarget.value})
-//     } 
-//     componentDidUpdate (prevProps, prevState) { 
-//         if(prevProps.status !== this.props.status){ 
-//             this.setState({ 
-//                 status: this.props.status
-//             })
-//         }
-//     }
-//     render(){ 
 
-//         return( 
-//             <div> 
-//                 {!this.state.editMode &&  <div onDoubleClick={this.activedEditMode}>{this.props.status || 'no Status'}</div>} 
-//                 {this.state.editMode &&  <div><input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deactivedEditMode} type="text" value={this.state.status} /></div>} 
-            
-//         </div>
-//     )
-// } 
-// } 
+import { useEffect, useState } from "react"
 const ProfileStatus = props =>{  
     const [editMode, setEditMode] = useState(false) 
     const [status, setStatus] = useState(props.status) 
