ff619a71243f0971a99b8f22daa8cde661e93b28
Anixii <kenjebekoverbol53@gmail.com>
2023-05-21 19:57:00 +0600

    93pagination

--- src/components/Users/Paginator.jsx
+++ src/components/Users/Paginator.jsx
@@ -1,17 +1,38 @@
-import s from './Users.module.css' 
- const Paginator = props =>{ 
+import s from './Users.module.css'  
+import { useState } from 'react';
+ const Paginator = ({portionSize = 10,...props}) =>{  
+    console.log(props);
     let pagesCount = Math.ceil(props.totalUsers / props.pageSize)
     let pages = []; 
     for(let i = 1; i<= pagesCount; i++){ 
         pages.push(i)
     } 
-    return(  
-        <> 
-        {pages.map(item =>{ 
-            return(
-                <button onClick={() => {props.setCurrentPage(item)}} className={props.currentPage === item && s.selectedItem}>{item}</button>
-                )})} 
-        </>
-    )
+    let portionCount = Math.ceil(pagesCount / portionSize)
+    let [portionNumber, setPortionNumber] = useState(1)
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+    let rightPortionPageNumber = portionNumber * portionSize
+
+
+    return <div className={s.pagination}>
+        {portionNumber > 1 ?
+            <button onClick={() => {
+                setPortionNumber(portionNumber - 1)
+            }}>Previous</button> : null
+        }
+        {pages
+            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map((p, index) => {
+                return <button className={props.currentPage === p ? s.selectedPage : s.unSelectedPage}
+                onClick={(e) => {
+                  props.setCurrentPage(p)               
+                }}              
+                key={index}>{p}</button>
+            })}
+        {portionCount > portionNumber ?
+            <button onClick={() => {
+                setPortionNumber(portionNumber + 1)
+            }}>Next</button> : null
+        }
+    </div>
 } 
 export default Paginator
