2749e3e296d5f1df4f1a87f515b14d365270bd09
Anixii <kenjebekoverbol53@gmail.com>
2023-06-02 18:47:27 +0600

    97 added Profile Refactor

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,7 +2,11 @@ import Preloader from '../../common/Preloader'
 import s from './ProfileInfo.module.css'
 import ProfileStatus from './ProfileStatus'
 import userAnonimAvatar from '../../../assets/image/user.png'
+import { useState } from 'react' 
+import ProfileForm from './ProfileForm'
 function ProfileInfo({ profile, status, ...props }) {
+     
+    const [edit, setEdit] = useState(false) 
     if (!profile) {
         return <Preloader />
     }
@@ -13,11 +17,21 @@ function ProfileInfo({ profile, status, ...props }) {
     }
     return (
 
-        <div className={s.info}>
-
-            <div> NickName: {profile.fullName}</div>
+        <div className={s.info}> 
             <div className={s.item}> <img className={s.ava} src={profile.photos.large || userAnonimAvatar} alt='Ava'></img></div>
-            <div>{props.isOwner && <input type='file' onChange={onPhotoSelected} />}</div>
+            <div>{props.isOwner && <input type='file' onChange={onPhotoSelected} />}</div>  
+            {edit ? <ProfileForm/> :<ProfileData profile={profile} isOwner={props.isOwner} handleEdit={() => setEdit(true)}/>}
+            
+            <ProfileStatus status={status} updateStatusThunkC={props.updateStatusThunkC} />
+        </div>
+    )
+}  
+const ProfileData = ({profile, isOwner, handleEdit}) =>{ 
+    return( 
+        <div>  
+            {isOwner && <button onClick={handleEdit}>Edit</button>}
+             <div> NickName: {profile.fullName}</div>
+            
             <ul> 
                 <li>Имя: {profile.fullName}</li>
                 <li> Обо мне: {profile.aboutMe}</li>
@@ -25,16 +39,14 @@ function ProfileInfo({ profile, status, ...props }) {
                 {profile.lookignForAJob && <li> 
                 Скилы: {profile.lookignForAJobDescription}     
                 </li>}
-             </ul>
-
-            <ProfileStatus status={status} updateStatusThunkC={props.updateStatusThunkC} />
-            <div>Контакты:
+             </ul> 
+             <div>Контакты:
                 {Object.keys(profile.contacts).map((item,index) =><Contacts key={index} title={item} value={profile.contacts[item]}/>)}
             </div>
 
         </div>
     )
-} 
+}
 const Contacts = ({title, value}) =>{ 
     return( 
         <>  
