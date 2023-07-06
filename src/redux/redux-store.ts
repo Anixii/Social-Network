cbaad181efa890b3ad464ddffe388702116fb5c2
Anixii <kenjebekoverbol53@gmail.com>
2023-07-06 17:16:26 +0600

    05/USers TS

---
+++ src/redux/redux-store.ts
@@ -0,0 +1,21 @@
+import {applyMiddleware, combineReducers,  legacy_createStore as createStore} from 'redux'
+
+import dialogsReducer from "./dialogsReducer"
+import profileReducer from "./profileReducer"
+import usersReducer from './usersReducer'
+import authreducer from './auth-reducer'
+import thunk from 'redux-thunk'
+import appReducer from './app-reducer'
+let reducers = combineReducers({  
+    postPage:profileReducer ,
+    dialogPage:dialogsReducer, 
+    usersPage: usersReducer, 
+    auth: authreducer,  
+    app: appReducer
+}) 
+type RootReducerType = typeof reducers 
+export type AppStateType = ReturnType<RootReducerType> 
+
+let store = createStore(reducers, applyMiddleware(thunk))  
+
+export default store
