849b73b1e49ae2e99e4fb9b6eb774e7e9b7686f5
Anixii <kenjebekoverbol53@gmail.com>
2023-07-09 23:54:21 +0600

    12/Unfollow Test Unit Jest TS

--- src/redux/users-reducer.test.tsx
+++ src/redux/users-reducer.test.tsx
@@ -1,7 +1,7 @@
 import usersReducer, { InitialStateType, actions } from "./usersReducer"
-
-test("User-Reducer-Test/FollowSucces", () => { 
-    const state:InitialStateType = {
+let state:InitialStateType 
+beforeEach(() => 
+    state = {
         users: [ 
             {id:0, name:'ANixii', followed:false, 
             status:'no status', photos:{small:null, large:null}
@@ -24,8 +24,16 @@ test("User-Reducer-Test/FollowSucces", () => {
             isFollowing: false,
             userId: 0
         } 
-    } 
+    }      
+)
+test("User-Reducer-Test/FollowSucces", () => { 
     const newState = usersReducer(state,actions.followAC(1)) 
     expect(newState.users[0].followed).toBeFalsy() 
     expect(newState.users[1].followed).toBeTruthy()
-})
+}) 
+ 
+test("User-Reducer-Test/UnFollowSucces", () => { 
+    const newState = usersReducer(state,actions.unfollowAC(3)) 
+    expect(newState.users[2].followed).toBeTruthy() 
+    expect(newState.users[3].followed).toBeFalsy()
+}) 
