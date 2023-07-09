89a8a63b107afe156f01813b7e3c51f2e0f6ec5c
Anixii <kenjebekoverbol53@gmail.com>
2023-07-09 22:57:27 +0600

    Preloader in tTS

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -2,5 +2,9 @@ declare module "*.module.css";
 declare module "*.module.scss"; 
 declare module "*.png" {
     const value: any;
+    export  default value;
+  } 
+  declare module "*.gif" {
+    const value: any;
     export default value;
   }
--- src/components/common/Preloader.tsx
+++ src/components/common/Preloader.tsx
@@ -1,5 +1,5 @@
 import loaderGif from '../../assets/image/loader.gif'
- export default function Preloader(props){ 
+ export default function Preloader(){ 
     return( <div>
         <img src={loaderGif} alt=''/> 
         </div>
